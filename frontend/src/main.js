9eaf979576744e0671b0d6fb4584743b4a4813c6
Matteo D <alex3025game@gmail.com>
2021-10-24 15:23:25 +0200

    Sistemata la base ed aggiunti alcuni plugin per semplificare le cose

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { setupLayouts } from "virtual:generated-layouts";
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import generatedRoutes from "virtual:generated-pages";
+
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: setupLayouts(generatedRoutes),
+});
+
+const app = createApp(App)
+app.use(router)
+
+app.mount('#app')
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Pages from "vite-plugin-pages";
+import Layouts from "vite-plugin-vue-layouts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    Pages(),
+    Layouts()
+  ],
+});
