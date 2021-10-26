bac71ce5c9c22b6743a55f851e6df7816b8d346e
Matteo D <matteodiblas82@gmail.com>
2021-10-26 10:56:59 +0200

    Installato WindiCSS come framework per l'UI.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import { setupLayouts } from "virtual:generated-layouts";
 
+import "virtual:windi.css";
+
 import App from './App.vue'
 import generatedRoutes from "virtual:generated-pages";
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from "vite-plugin-pages";
 import Layouts from "vite-plugin-vue-layouts";
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Pages(),
-    Layouts()
+    Layouts(),
+    WindiCSS()
   ],
 });
