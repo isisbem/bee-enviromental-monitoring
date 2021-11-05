2757389688a3df4a629c1e9c963dfc5a419e5f6f
Antonio I <78216046+antoniognogno@users.noreply.github.com>
2021-11-05 10:17:53 +0100

    Modifiche faq/aboutus

--- frontend/src/pages/about.vue
+++ frontend/src/pages/about.vue
@@ -2,7 +2,7 @@
   <div class="sm:flex flex-col justify-center items-center text-center h-screen flex-grow">
     
     <img src="/src/assets/images/esempio_gruppo.jfif" class="w-[1000px] h-screen">
-    <h1>
+    <h1 class="mt-50">
       siamo un gruppo molto bello giwncibjaicjbnuaibuicbiuabiufba
     </h1>
       </div>
--- frontend/src/pages/faq.vue
+++ frontend/src/pages/faq.vue
@@ -18,41 +18,17 @@
 </script>
 
 <template>
-  <main class="mt-5 mx-auto max-w-7xl px-4 sm:mt-6 sm:px-6 md:mt-8 lg:mt-10 lg:px-8 xl:mt-19">
-    <div class="sm:flex flex-col justify-center items-center text-center">
-      <h1 class="text-3xl tracking-tight font-extrabold flex justify-center items-center sm:text-4xl md:text-5xl">
-        <span class="block text-yellow-500">Frequently</span>
-        <span class="block text-black">Asked</span>
-        <span class="block text-yellow-500">Questions</span>
-      </h1>
-      <span class="mt-10 h-px w-full bg-gray-300" />
-      <div class="mt-3 mx-auto max-w-7xl px-4 sm:mt-6 sm:px-6 md:mt-8 lg:mt-10 lg:px-8 xl:mt-17">
-      <button class="accordion w-full bg-yellow-600 rounded py-1">Domanda 1</button>
-        <div class="py-3 bg-white overflow: hidden;">
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti neque aperiam mollitia nobis esse ullam magni magnam a at voluptatibus vero, doloribus ipsa voluptatum qui? Provident facilis sed tenetur aliquam?</p>
-        </div>
-
-        <button class="accordion w-full bg-yellow-600 rounded py-1">Domanda 2</button>
-        <div class="py-3 bg-white overflow: hidden;">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quo saepe fugiat corrupti molestias dolores optio delectus voluptatibus ad doloremque sequi odit, expedita, excepturi veritatis neque magnam, eveniet exercitationem deserunt?</p>
-        </div>
-
-        <button class="accordion w-full bg-yellow-600 rounded py-1">Domanda 3</button>
-        <div class="py-3 bg-white overflow: hidden;">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quo saepe fugiat corrupti molestias dolores optio delectus voluptatibus ad doloremque sequi odit, expedita, excepturi veritatis neque magnam, eveniet exercitationem deserunt?</p>
-        </div>
-        
-        <button class="accordion w-full bg-yellow-600 rounded py-1">Domanda 4</button>
-        <div class="py-3 bg-white overflow: hidden;">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quo saepe fugiat corrupti molestias dolores optio delectus voluptatibus ad doloremque sequi odit, expedita, excepturi veritatis neque magnam, eveniet exercitationem deserunt?</p>
-        </div>
-        
-        <button class="accordion w-full bg-yellow-600 rounded py-1">Domanda 4</button>
-        <div class="py-3 bg-white overflow: hidden;">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quo saepe fugiat corrupti molestias dolores optio delectus voluptatibus ad doloremque sequi odit, expedita, excepturi veritatis neque magnam, eveniet exercitationem deserunt?</p>
-        </div>
-
+  <main class="mx-auto mt-4 max-w-7xl px-4 sm:mt-6 sm:px-6 md:mt-8 lg:mt-10 lg:px-8 xl:mt-19">
+    <div class="grid-cols-5 flex flex-auto sm:flex">
+      <h3 class="text-3xl tracking-tight font-extrabold sm:text-base md:text-4xl">
+        <span class="block text-center text-black">Frequently</span>
+        <span class="block text-center text-yellow-500">Asked</span>
+        <span class="block text-center text-black">Questions</span>
+      </h3>
+      <div class="flex flex-col">
+        <h4 class="text-base text-gray-600 tracking-tight sm:text-lg md:text-xl">Qui puoi trovare le domande più frequenti che ci vengono poste, se ne hai altre non esitare a contattarci</h4>
       </div>
+    <!--<img class="absolute bottom-20 h-50 sm:flex" src="../assets/images/undraw_faq.png">-->
     </div>
   </main>
 </template>
