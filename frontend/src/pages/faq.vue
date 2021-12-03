a18885250281adc4de0f154122c47b3e46dfd2a7
Antonio I <78216046+antoniognogno@users.noreply.github.com>
2021-12-03 08:38:15 +0100

    Backup 03/12/2021
    Un commit solo per salvare il lavoro svolto fin ora di impostazione

--- frontend/src/pages/faq.vue
+++ frontend/src/pages/faq.vue
@@ -1,22 +1,15 @@
 
 <script>
-/*  var acc = document.getElementsByClassName("accordion");
-  var i;
-
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-
-      var panel = this.nextElementSibling;
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-      } else {
-        panel.style.display = "block";
-      }
-    });
-  }*/
+  export default {
+    components: {
+      Disclosure,
+      DisclosureButton,
+      DisclosurePanel,
+      ChevronUpIcon,
+    },
+  }
 </script>
-
+  
 <template>
   <main class="mx-auto mt-4 max-w-7xl px-4 sm:mt-6 sm:px-6 md:mt-8 lg:mt-10 lg:px-8 xl:mt-19">
     <div class="grid-cols-5 flex flex-auto sm:flex">
@@ -25,10 +18,12 @@
         <span class="block text-center text-yellow-500">Asked</span>
         <span class="block text-center text-black">Questions</span>
       </h3>
-      <div class="flex flex-col">
-        <h4 class="text-base text-gray-600 tracking-tight sm:text-lg md:text-xl">Qui puoi trovare le domande più frequenti che ci vengono poste, se ne hai altre non esitare a contattarci</h4>
+      <div class="flex flex-col"> 
+        <h4 class="text-base text-gray-700 tracking-tight m-4 sm:text-lg md:text-xl">Qui puoi trovare le domande più frequenti che ci vengono poste, se ne hai altre non esitare a contattarci</h4>
       </div>
-    <!--<img class="absolute bottom-20 h-50 sm:flex" src="../assets/images/undraw_faq.png">-->
     </div>
   </main>
 </template>
+  
+    <!--<img class="absolute bottom-20 h-50 sm:flex" src="../assets/images/undraw_faq.png">-->
+
