d5964c4894bd7c41fc28dfc30852997a2bd58ca6
CrisJuve99 <radevcristian@gmail.com>
2021-12-09 15:34:16 +0100

    about us 3.0

--- frontend/src/pages/about.vue
+++ frontend/src/pages/about.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="mt-20 sm:flex flex-col justify-center items-center text-center h-screen flex-grow">
-    
-    <img src="/src/assets/images/prova.jpg" class="h-150 w-320" >
-    <p class="mt-25 text-center mx-50">
-      siamo un gruppo molto bello siamo un gruppo molto bello siamo un gruppo molto bello siamo un gruppo molto bello siamo un gruppo molto bellosiamo un gruppo molto bellosiamo un gruppo molto bellosiamo un gruppo molto bellosiamo un gruppo molto bellosiamo un gruppo molto bellosiamo un gruppo molto bellosiamo un gruppo molto bello  
-    </p>
-  </div>
+  <main >
+    <div class="mt-20 sm:flex flex-col justify-center items-center text-center h-screen flex-grow">  
+      <img src="/src/assets/images/papesca1-320x150.jpg" class="h-110 w-300" >
+      <p class="mt-25 text-center mx-50 font-bold ">
+        Il nostro gruppo è parte della scuola Guglielmo Marconi di Staranzano (GO). L’obiettivo di tale progetto è quello di monitorare la qualità di vita terrena delle api, uno degli insetti più sofisticati e importanti al mondo. Sfruttando le nostre competenze informatiche abbiamo cercato di sviluppare un sistema, che attraverso l'utilizzo di schede implementate di sensori, rilevano l’ingresso dell’insetto all’interno del proprio alveare. Il loro monitoraggio è posto di vitale importanza per garantire un futuro più roseo e prosperoso al pianeta. Senza un monitoraggio adeguato si rischierebbe di incappare in errori fatti nel passato da persone che hanno anteposto la loro sopravvivenza personale a quella del pianeta. Oramai è risaputo come le api garantiscano un bilanciamento ottimale al nostro pianeta e, senza la loro esistenza, non potremmo vivere tutti per molti anni. Stiamo lavorando per restituire ed istituire una coscienza negli uomini di fondamentale conoscenza per un mondo green.  
+      </p>
+    </div>
+  </main>
 </template>
