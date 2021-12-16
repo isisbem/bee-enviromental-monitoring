c272de08538b16f75bdfde43ea0bc6c4382d0b71
Milos Kovacevic <misi.kovacevic@gmail.com>
2021-12-16 15:56:08 +0100

    Modifiche about us

--- frontend/src/pages/about.vue
+++ frontend/src/pages/about.vue
@@ -1,10 +1,13 @@
 <template>
   <main >
     <div class="mt-20 sm:flex flex-col justify-center items-center text-center h-screen flex-grow">  
-      <img src="/src/assets/images/papesca1-320x150.jpg" class="h-110 w-300" >
-      <p class="mt-25 text-center mx-50 font-bold ">
-        Il nostro gruppo è parte della scuola Guglielmo Marconi di Staranzano (GO). L’obiettivo di tale progetto è quello di monitorare la qualità di vita terrena delle api, uno degli insetti più sofisticati e importanti al mondo. Sfruttando le nostre competenze informatiche abbiamo cercato di sviluppare un sistema, che attraverso l'utilizzo di schede implementate di sensori, rilevano l’ingresso dell’insetto all’interno del proprio alveare. Il loro monitoraggio è posto di vitale importanza per garantire un futuro più roseo e prosperoso al pianeta. Senza un monitoraggio adeguato si rischierebbe di incappare in errori fatti nel passato da persone che hanno anteposto la loro sopravvivenza personale a quella del pianeta. Oramai è risaputo come le api garantiscano un bilanciamento ottimale al nostro pianeta e, senza la loro esistenza, non potremmo vivere tutti per molti anni. Stiamo lavorando per restituire ed istituire una coscienza negli uomini di fondamentale conoscenza per un mondo green.  
-      </p>
+      <img src="../assets/images/coso.jpeg" class="h-110 w-300" >
+      <div class="container mt-15 text-center font-bold box-border h-57 w-250 p-4 border-4 rounded-lg border-dark-500">
+          state chiedendo chi siamo, bene. Noi siamo una classe quarta di un istituto tecnico che si sta 
+          occupando di questo progetto. Questo progetto è un progetto che ha come scopo la supervisione della
+          vita delle api sfruttando delle schede di acquisizione dati raccogliamo dati d’entrata e uscita 
+          tramite  sensori posti su di esse.
+      </div>
     </div>
   </main>
 </template>
