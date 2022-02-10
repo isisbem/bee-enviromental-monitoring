05231c850b91cf1348ed1ba61ce1aeeee477e907
CrisJuve99 <radevcristian@gmail.com>
2022-02-10 15:54:16 +0100

    aggiunta flip e modifiche

--- frontend/src/pages/about.vue
+++ frontend/src/pages/about.vue
@@ -8,11 +8,56 @@
           Guglielmo Marconi, di Staranzano. Ci è stato affidato il compito di gestire  
           </p>
       </div>
-      <div class = "flex flex-row space-x-6">
-        <div><img class="rounded-lg hover:rotate-90" src="../assets/images/immagine about prova.jpeg" ></div>
-        <div><img class="rounded-lg" src="../assets/images/immagine about prova.jpeg" ></div>
-        <div><img class="rounded-lg" src="../assets/images/immagine about prova.jpeg" ></div>
+      <div class="grid gap-20 mb-10 w-40 h-40">  
+        <div class="flip-box">
+          <div class="flip-box-inner relative">
+            <div class="flip-box-front absolute w-50 h-50">
+              <img class="rounded-lg" src="../assets/images/immagine about prova.jpeg">
+            </div>
+            <div class="flip-box-back text-align-center">
+              <h2 class="text-color-black dark:text-white">Paris</h2>
+              <p class="text-color-black dark:text-white">What an amazing city</p>
+            </div>
+          </div>
+        </div>
       </div>
+
+     <!--   <div class = "flex flex-row space-x-6 flip-box-back">
+          <h2>Prova</h2>
+          <p>questa è una prova, minchia</p>
+          <div><img class="rounded-lg" src="../assets/images/immagine about prova.jpeg" ></div>
+          <div><img class="rounded-lg" src="../assets/images/immagine about prova.jpeg" ></div>
+          <div><img class="rounded-lg" src="../assets/images/immagine about prova.jpeg" ></div>
+      </div>
+        <div class = "flex flex-row space-x-6">
+          <div><img class="rounded-lg" src="../assets/images/immagine about prova.jpeg" ></div>
+          <div><img class="rounded-lg" src="../assets/images/immagine about prova.jpeg" ></div>
+          <div><img class="rounded-lg" src="../assets/images/immagine about prova.jpeg" ></div>
+        </div>
+    </div> -->
     </div>
   </main>
 </template>
+
+
+<style>
+
+.flip-box-inner {
+  transition: transform 0.5s;
+  transform-style: preserve-3d;
+}
+
+.flip-box:hover .flip-box-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-box-front, .flip-box-back {
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.flip-box-back {
+  transform: rotateY(180deg);
+}
+</style>
+
