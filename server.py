4a2a30d6ddd1f82cc9d2d71a790bfd2030c8b5f9
Antonio I <78216046+antoniognogno@users.noreply.github.com>
2023-02-24 15:50:46 +0100

    Faq

--- server.py
+++ server.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
