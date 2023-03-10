3b5cd39724850def20c0c0acae44e24b7431012a
Matteo D <alex3025game@gmail.com>
2023-03-10 15:55:28 +0100

    contatti & chi siamo

--- server.py
+++ server.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/contatti')
+def contatti():
+    return render_template('contatti.html')
+
+
+@app.route('/chi_siamo')
+def chi_siamo():
+    return render_template('chi_siamo.html')
+
+
+@app.route('/ipanel')
+def ipanel_static():
+    return render_template('ipanel/static.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
