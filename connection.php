3029c1d7b65ab7446f61195b8fce2f8c37085f9e
Antonio <aiorio2004@gmail.com>
2023-05-08 09:22:36 +0200

    Database collegato
    Collegato database con libreria MySql e Python/Mysqli e PHP

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+$con = mysqli_connect("82.223.8.163", "obt", "Obt!2023", "OBT", "53306");
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+?>
